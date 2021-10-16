069d0d4fa44b724db32a18c29176a3a45c7f5f28
msaaddev <mrsaadirfan@gmail.com>
2021-10-17 03:47:55 +0500

    👌 IMPROVE: Header component

--- components/common/header.js
+++ components/common/header.js
@@ -1,15 +1,28 @@
 import Link from 'next/link';
 import styles from '../../styles/header.module.css';
 
-const Header = ({ text }) => {
+const Header = ({ text, working, word }) => {
 	return (
 		<div className={styles.container}>
-			<Link href="/">
-				<h2>
-					Image <span>Optimization</span> With Next.js
-				</h2>
-			</Link>
-			<h3>{text}</h3>
+			{!working ? (
+				<>
+					<Link href="/">
+						<h2>
+							Image <span>Optimization</span> With Next.js
+						</h2>
+					</Link>
+					<h3>{text}</h3>
+				</>
+			) : (
+				<>
+					<Link href={`/${word}`}>
+						<h2>
+							Working of <span>{word}</span>{' '}
+							{word !== 'tag' ? 'Component' : 'tag'}
+						</h2>
+					</Link>
+				</>
+			)}
 		</div>
 	);
 };
