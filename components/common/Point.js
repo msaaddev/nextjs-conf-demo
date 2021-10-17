fc0362786ff13133730ecb17075a076e6065e372
msaaddev <mrsaadirfan@gmail.com>
2021-10-17 19:49:14 +0500

    📦 NEW: Dynamic data

--- components/common/Point.js
+++ components/common/Point.js
@@ -3,10 +3,7 @@ import styles from '../../styles/points.module.css';
 const Point = ({ info }) => {
 	return (
 		<div className={styles.point}>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry.
-			</p>
+			<p>{info}</p>
 		</div>
 	);
 };
